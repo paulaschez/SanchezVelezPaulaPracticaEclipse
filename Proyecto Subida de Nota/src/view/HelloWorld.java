3759254f1d2b18d221fd6a4a7ef6a39f970a5768
paulaschez <psanchezvelez03@gmail.com>
2024-05-19 21:41:30 +0200

    Volvemos a modificar el mensaje desde GitHub

--- Proyecto Subida de Nota/src/view/HelloWorld.java
+++ Proyecto Subida de Nota/src/view/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 	// Tu programa comienza con una llamada a main().
 	// Imprime "Hola Mundo" a la ventana de la terminal.
 	public static void main(String args[]) {
-		System.out.println("Hola Mundo. Soy Paula");
+		System.out.println("Hola Mundo desde GitHub");
 	}
 }
